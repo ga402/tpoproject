014dfc15eb7d741e60abc8cb639e9a745e5badd4
ga402 <george.adams02@imperial.ac.uk>
2018-07-16 13:01:54 +0100

    major update

--- Bayes_model_TPO.R
+++ Bayes_model_TPO.R
@@ -8,129 +8,125 @@ library(rethinking)  # useful package for bayesian models
 ##
 ## -------------------------------------------------------##
 
-# To work - the model needs to have gaussian distributed values
-# to achieve this - log normalisation/transformation
+## model
 
-## MODEL 1: ITP patients
-
-# d1 - itp patients
-# flat priors
-
-m2 <- map(
+m2 <- map2stan(
   alist(
-    plt.log ~ dnorm(mu, sigma),
-    mu <- a + b*tpo.log,
-    a ~ dnorm(4,2),
-    b ~ dnorm(0, 10),
-    sigma ~ dunif(-1,1)), 
-  data = d1)
-
-## Extraction - model 1 - ITP patients
-post1 <- extract.samples(m2, n = 1000)
-
-# Prediction for platelet counts
-mu.10 <- post1$a + exp(post1$b*10)
-mu.1 <- post1$a + exp(post1$b*1)
-mu.150 <- post1$a + exp(post1$b*150)
-mu.2 <- post1$a + exp(post1$b*2)
-mu.3 <- post1$a + exp(post1$b*3)
-mu.4 <- post1$a + exp(post1$b*4)
-mu.5 <- post1$a + exp(post1$b*5)
-mu.6 <- post1$a + exp(post1$b*6)
-mu.7 <- post1$a + exp(post1$b*7)
-mu.8 <- post1$a + exp(post1$b*8)
-mu.9 <- post1$a + exp(post1$b*9)
+    tpo_log ~ dnorm(mu, sigma),
+    mu <- a + (b*plt_log),
+    a ~ dnorm(0.01,10),
+    b ~ dnorm(0.01,10),
+    tpo <- exp(tpo_log),
+    plt <- exp(plt_log),
+    sigma ~ dunif(-1,10)), 
+  data = d3, warmup = 500, iter = 1e5)
 
 
+## predict outcomes
+plt <- seq(from=1,to=200,length.out=200) ## plt counts you want to predict
+new_data <- data.frame(plt_log=log(plt))  ## new dataset of plt counts
+lambda <- link(m2,data=new_data,n=9000)   ## project model parameters - project 9000
 
-## MODEL 2: non-itp patients
+lambda.mu.mean <- apply(lambda$mu,2,mean)  # take average of each column to get mean
+lambda.mu.HPDI <- apply(lambda$mu,2,HPDI)  # highest posterior density (HPDI) and percentile (PI)
+head(lambda.mu.mean)
+tt.sim <- sim(m2,data=new_data,n=9000)  # simulate out the model to get simulated curves
+tt.HPDI <- apply(tt.sim,2,HPDI)  ## highest posterior density simulations
+tt.HPDI
 
-d2 <- list(plt.log = nitp$plt.log,
-           tpo.log = nitp$tpo.log)
-
-# flat priors
-
-m3 <- map(
-  alist(
-    plt.log ~ dnorm(mu, sigma),
-    mu <- a + b*tpo.log,
-    a ~ dnorm(3,1),
-    b ~ dnorm(0, 10),
-    sigma ~ dunif(1,10)), 
-  data = d2)
-
-
-# extraction /prediction for model 2
-post2 <- extract.samples(m3, n = 1000)
-
-
-# prediction for different platelet counts
-mu10 <- post2$a + exp(post2$b*10)
-mu1 <- post2$a + exp(post2$b*1)
-mu150 <- post2$a + exp(post2$b*150)
-mu2 <- post2$a + exp(post2$b*2)
-mu3 <- post2$a + exp(post2$b*3)
-mu4 <- post2$a + exp(post2$b*4)
-mu5 <- post2$a + exp(post2$b*5)
-mu6 <- post2$a + exp(post2$b*6)
-mu7 <- post2$a + exp(post2$b*7)
-mu8 <- post2$a + exp(post2$b*8)
-mu9 <- post2$a + exp(post2$b*9)
+## plot predictions - 
+par(mfrow = c(1,1))
+par(mar = c(4,4,4,4))
+plot( tpo_log ~ plt , data=d3 , pch=16 , col=rangi2,
+      ylab = "log TPO pg/ml", 
+      xlab = (expression(paste("Platelet count (",x10^{9},"/L)"))), mgp = c(2,0.5,0))
+lines( plt , lambda.mu.mean )
+shade( lambda.mu.HPDI , plt )
+shade( tt.HPDI , plt )
 
 
-## reverse transformation and subset into a dataframe
-df2 <- data.frame(
-  "non-itp1" = exp(mu1),
-  "non-itp2" = exp(mu2),
-  "non-itp3" = exp(mu3),
-  "non-itp4" = exp(mu4),
-  "non-itp5" = exp(mu5),
-  "non-itp6" = exp(mu6),
-  "non-itp7" = exp(mu7),
-  "non-itp8" = exp(mu8),
-  "non-itp9" = exp(mu9),
-  "non-itp10" = exp(mu10),
-  "non-itp150" = exp(mu150),
-  "itp1" = exp(mu.1) ,
-  "itp2" = exp(mu.2) ,
-  "itp3" = exp(mu.3) ,
-  "itp4" = exp(mu.4) ,
-  "itp5" = exp(mu.5) ,
-  "itp6" = exp(mu.6) ,
-  "itp7" = exp(mu.7) ,
-  "itp8" = exp(mu.8) ,
-  "itp9" = exp(mu.9) ,
-  "itp10" = exp(mu.10) ,
-  "itp150" = exp(mu.150),
-  "number" = seq(1, 1000, 1))
+## pull results into a dataframe
+head(tt.HPDI)
 
-# reshape dataset
-df3 <- reshape(df2, varying = c(1:22), idvar = "number", direction = "long", sep = "")
-
-## Further adjustment for plotting
-
-df3$non.itp <- as.vector(df3$non.itp) # vector
-sum(df3$non.itp == "Inf")             # remove "INF" results
-colnames(df3)[2] <- "platelet_count"  # rename columns
-
-df4 <- reshape(df3, varying = c(3:4),idvar = "tpo",direction= "long") ## further reshape to single column
+df4 <- tt.sim[,c(1,2,3,4,5,6,7,8,9,10, 20, 30, 40, 50, 60, 70, 80, 90, 150, 200)]
+df4 <- as.data.frame(df4)
 head(df4)
+n1 <-sprintf("Platelet_count%s", c(1,2,3,4,5,6,7,8,9,10, 20, 30, 40, 50, 60, 70, 80, 90, 150, 200))
+colnames(df4) <- n1  # labels
 
-## Form dataset
-df5 <- tibble("tpo" = c(df4$non, df4$itp),
-              "diagnosis" = c(rep("BMF", 11000),rep("itp", 11000)),
-              "platelet_count" = rep(df4$platelet_count, 2))
 
-## diagnosis as factor - to allow plotting
-df5$d <- as.factor(df5$diagnosis)
+#write.table(df4, "df4.tsv", col.names = NA, quote = F, sep = "\t")
 
-sum(df5$tpo == "Inf")  # check infinity removed
+# reshape
+df5 <- reshape(df4, varying = c(1:20), idvar = "number", direction = "long", sep = "")
 
-## 25 rwos in df$tpo (due to non-itp) that have an infinite result - we must remove these
-df6 <- df5[!df5$tpo == "Inf", ]  # remove "Inf" values"
-df7 <- df6[!df6$tpo > 2000,]   # remove extreme values!
+colnames(df5) <- c("Platelet_count", "tpo", "number")  # colnames
+#write.table(df5, "df5.tsv", col.names = NA, quote = F, sep = "\t")
 
-## further subset for only platelet counts 1, 10 and 150
-df8 <- df7[df7$platelet_count == 1 | df7$`platelet count` == 10 |
-             df7$platelet_count == 150,]
+df5$tpo_e <- exp(df5$tpo)# add an tpo (instead of log tpo)
+
+## density plot
+ggdensity(df5, x = "tpo", y = "..density..", combine = T, rug = TRUE, fill = "Platelet_count",
+          color = 'Platelet_count', palette = "simpsons", add = 'mean')
+
+df5$Platelet_count <- as.character(df5$Platelet_count)
+
+## subset a few levels
+df6 <- df5[df5$Platelet_count == 1 | df5$Platelet_count == 10 | df5$Platelet_count == 20 | df5$Platelet_count == 30 | 
+             df5$Platelet_count == 40 | df5$Platelet_count == 50 | df5$Platelet_count == 60 | 
+             df5$Platelet_count == 70 | df5$Platelet_count == 80 | df5$Platelet_count == 90 |
+             df5$Platelet_count == 100,]
+
+
+ggdensity(df6, x = "tpo_e", y = "..density..", combine = T, rug = TRUE, fill = "Platelet_count",
+          color = 'Platelet_count', palette = "grey", add = 'mean', xlim = c(0, 1000))
+
+dipt1 <- ggdensity(df6, x = "tpo_e", y = "..density..", combine = T, rug = TRUE, fill = "Platelet_count",
+                   color = 'Platelet_count', palette = "grey", add = "median", xlab = "TPO (pg/ml)",
+                   label.select = list(top.up = 200, 20, 10, 7, 5, 3, top.down = 1), xlim = c(0, 1000))
+
+
+## plot
+
+par(mar = c(4,4,4,4))
+plot( tpo_log ~ plt , data=d3 , pch=16 , col=rangi2,
+      ylab = "log TPO pg/ml", 
+      xlab = (expression(paste("Platelet count (",x10^{9},"/L)"))), mgp = c(2,0.5,0))
+lines( plt , lambda.mu.mean )
+shade( lambda.mu.HPDI , plt )
+
+## make a new dataframe of plt versys log tpo
+d9 <- data.frame(
+  "plt" = plt,
+  "log_tpo" = lambda.mu.mean
+)
+
+head(d9)
+class(d9)
+
+shade( tt.HPDI , plt )
+head(tt.HPDI)
+
+head(lambda.mu.HPDI)
+n1 <- sprintf("Platelet_count%s", c(1:200))
+
+d7 <- as.data.frame(tt.HPDI)
+dim(d7)
+colnames(d7) <- n1
+rownames(d7) <- c("lower", "upper")
+
+d8 <- reshape(d7, varying = c(1:200), idvar = "number", direction = "long", sep = "")
+colnames(d8) <- c("Platelet_count", "tpo_log", "dim")
+head(d8)
+d8$tpo <- exp(d8$tpo_log)
+head(d8)
+
+## write the dataframes
+d5 <- reshape(d4, idvar = 1:200,direction =  "long")
+write.table(d5, "dim_d5.tsv", col.names = NA, quote = F, sep = "\t")
+write.table(d8, "dim_d8.tsv", col.names = NA, quote = F, sep = "\t")
+
+
+
+
 
